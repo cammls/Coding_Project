1fda34550015d396b0793ebcbfc3d42ef939048b
cammls <camille.malis@coding-academy.fr>
2017-01-31 14:41:09 +0100

    package.json corrected, register improved

--- app.js
+++ app.js
@@ -15,41 +15,47 @@ var neo4j = require('neo4j-driver').v1;
 var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "camille"));
 var session = driver.session();
 //login !!
-app.post('/api/login', function(req, res){
-    console.log("1");
-  passport.use(new LocalStrategy(function(username, password, done) {
-      console.log("2");
-      session
-      .run("MATCH (u:User) WHERE u.username = {username} RETURN u", {username: req.body.username})
-      .then(function(result){
-        if (result == null){
-          return done(res.send("incorrect username"))
-          // return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!bcrypt.compareSync(req.body.password, hash)) // true)
-        {
-          // return done(null, false, { message: 'Incorrect password.' });
-          return done(res.send("incorrect username"))
-        }
-      });
-        return done(null, user);
-    }
-  ))
-});
+// app.post('/api/login', function(req, res){
+//     console.log("1");
+//   passport.use(new LocalStrategy(function(username, password, done) {
+//       console.log("2");
+//       session
+//       .run("MATCH (u:User) WHERE u.username = {username} RETURN u", {username: req.body.username})
+//       .then(function(result){
+//         if (result == null){
+//           return done(res.send("incorrect username"))
+//           // return done(null, false, { message: 'Incorrect username.' });
+//         }
+//         if (!bcrypt.compareSync(req.body.password, hash)) // true)
+//         {
+//           // return done(null, false, { message: 'Incorrect password.' });
+//           return done(res.send("incorrect username"))
+//         }
+//       });
+//         return done(null, user);
+//     }
+//   ))
+// });
 // CRUD users routes
 //Create / Register
 app.post('/api/users', function (req, res) {
   var hash = bcrypt.hashSync(req.body.password);
   session
-  .run( "CREATE (:User {username: {username}, email: {email}, password: {password}})",
-  {username: req.body.username, email: req.body.email, password: hash })
+  .run( "CREATE (:User {firstname: {firstname}, lastname: {lastname}, role: {role}, email: {email}, password: {password}})",
+  {firstname: req.body.firstname,lastname: req.body.lastname, email: req.body.email,role: req.body.role, password: hash })
 
   .then( function() {
     res.send('Account created');
     res.end();
     session.close();
-    driver.close();
+    // driver.close();
+  })
+  .catch(function(error) {
+    res.send(error);
+    console.log(error);
   });
+
+
 });
 // List all users in json
 app.get('/api/users', function (req, res){
