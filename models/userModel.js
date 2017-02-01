3313001d9eb935ce3b90aa0d09f1c1e7d6245ca8
benoit nguyen <nguyen.hl.b@gmail.com>
2017-02-01 17:18:37 +0100

    created skeletons for register/login/logout

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,80 @@
-var driver    = require('./connect_db.js').connectdb()
-var session   = driver.session()
+var passport       = require('passport')
+var LocalStrategy  = require('passport-local').Strategy
+var path           = require('path')
+var jwt            = require('../services/generateJswt.js')
+var driver         = require('./connect_db.js').connectdb()
+var session        = driver.session()
 
-// Log user generate token
+var registerUser = function() {
+
+}
+
+var loginUser = function() {
+
+}
+
+var logoutUser = function() {
+
+}
+
+// var register = function(req, res) {
+//   console.log(req.body)
+//   var user = new User()
+//
+//   user.name = req.body.name;
+//   user.email = req.body.email;
+//
+//   user.setPassword(req.body.password)
+//   user.save(function(err) {
+//     if (err) throw err
+//     var token
+//     token = user.generateJswt()
+//     res.status(200)
+//     res.json({
+//       "token" : token
+//     });
+//   });
+// };
+
+// console.log(req.body)
+// passport.use(new LocalStrategy(function(username, password, done) {
+//     console.log("2");
+//     session
+//     .run("MATCH (u:User) WHERE u.username = {username} RETURN u", {username: req.body.username})
+//     .then(function(result){
+//       if (result == null){
+//         return done(res.send("incorrect username"))
+//         // return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!bcrypt.compareSync(req.body.password, hash)) // true)
+//       {
+//         // return done(null, false, { message: 'Incorrect password.' });
+//         return done(res.send("incorrect username"))
+//       }
+//     });
+//     return done(null, user);
+//   }
+// ))
+
+
+// passport.authenticate('local', function(err, user, info) {
+//   var token;
+//
+//   if (err) {
+//     res.status(404).json(err)
+//     return;
+//   }
+//   if (user) {
+//     token = user.generateJswt();
+//     res.status(200)
+//     res.json({
+//       "token" : token
+//     })
+//   } else {
+//     res.status(401).json(info)
+//   }
+// })(req, res)
+
+exports.registerUser = registerUser
+exports.loginUser = loginUser
+exports.logoutUser = logoutUser
--- services/generateJswt.js
+++ services/generateJswt.js
@@ -1,16 +1,18 @@
 var path      = require('path')
 var config    = require(path.join(__dirname, '../config/config.js'))
+var jwt       = require('jsonwebtoken')
 
-var generateJswt = function() {
+
+var generateJswt = function(id, email, first_name, last_name, role) {
   var expiry = new Date();
   expiry.setDate(expiry.getDate() + 7);
 
   return jwt.sign({
-    id: this.id,
-    email: this.email,
-    first_name: this.first_name,
-    last_name: this.last_name,
-    role: this.role,
+    id: id,
+    email: email,
+    first_name: first_name,
+    last_name: last_name,
+    role: role,
     exp: parseInt(expiry.getTime() / 1000),
   }, config.secret);
 }
