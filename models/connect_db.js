1d56f5302819da1d62a29d0cd31e0713833c8901
RayedB <rayed.benbrahim@gmail.com>
2017-02-01 16:02:45 +0100

    online graph db

--- models/connect_db.js
+++ models/connect_db.js
@@ -2,7 +2,7 @@ var config    = require('../config/config.js')
 var neo4j     = require('neo4j-driver').v1
 
 var connectdb = function() {
-  return neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", config.neo4jpassword))
+  return neo4j.driver("bolt://hobby-lakdnobeoeaggbkedbifhpol.dbs.graphenedb.com:24786", neo4j.auth.basic("neo4j", config.neo4jpassword))
 }
 
 exports.connectdb = connectdb
