a2d3eb99e9e3c6024af2ed34e69d53a8ef15872d
benoit nguyen <nguyen.hl.b@gmail.com>
2017-02-02 16:11:46 +0100

    correctly getting user after it is created

--- models/userModel.js
+++ models/userModel.js
@@ -3,17 +3,31 @@
 var path           = require('path')
 var jwt            = require('../services/jswtService.js')
 var driver         = require('./connect_db.js').connectdb()
+var neo4j          = require('neo4j-driver').v1
 var session        = driver.session()
 var bcrypt         = require('bcrypt-nodejs')
 
 var registerUser = function(user_data, callback) {
   var hashed_pass = bcrypt.hashSync(user_data.password)
-  // var token = jwt.generateJswt(user_data)
-  session.run("CREATE (p:User {first_name: {first_name}, last_name: {last_name}, role: {role}, email: {email}, password: {password}}) RETURN p",
+  session.run("CREATE (p:User {first_name: {first_name}, last_name: {last_name}, role: {role}, email: {email}, password: {password} }) RETURN ID(p), p.first_name, p.last_name, p.email, p.role",
               {first_name: user_data.first_name,last_name: user_data.last_name, email: user_data.email,role: user_data.role, password: hashed_pass })
          .then(function(result) {
-              console.log(result.records[0])
-              callback(result.records.keys)
+              var record = result.records[0]
+              var id = record._fields[0].low
+              var first_name = record._fields[1]
+              var last_name = record._fields[2]
+              var email = record._fields[3]
+              var role = record._fields[4]
+              var user_data = {id: id, first_name: first_name, last_name: last_name, email: email, role: role }
+              var token = jwt.generateJswt(user_data)
+              session.close()
+              // "MATCH (s) WHERE ID(s) = id RETURN s"
+              session.run("MATCH (n:User) WHERE ID(n) = {id} RETURN n", {id: neo4j.int(id)}).then(function(result) {
+                console.log(result.records[0]._fields)
+              }, function(reason) {
+                console.log(reason)
+              })
+              // callback(result.records)
           }, function(reason) {
               console.log(reason)
           });
