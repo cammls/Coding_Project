39edbc6a55161b8e253d6d3dfa682f5fac4cb9e7
cammls <camille.malis@coding-academy.fr>
2017-02-06 19:37:23 +0100

    stripe view works

--- public/controllers/financeController.js
+++ public/controllers/financeController.js
@@ -2,10 +2,11 @@ app.controller('financeCtrl', ['$scope','dataService', function($scope, dataServ
   $scope.getStripeData = function(){
     dataService.getStripeData()
     .then(function mySucces(response) {
-          // $scope.stripeData = response.data;
-          $scope.stripeBalance = response.data.balance;
-          $scope.stripeCharges = response.data.charges;
-          $scope.stripeCustomers = response.data.customers;
+          $scope.stripeData = response.data;
+          console.log(response.data);
+          // $scope.stripeBalance = response.data.balance;
+          // $scope.stripeCharges = response.data.charges;
+          // $scope.stripeCustomers = response.data.customers;
       }, function myError(error) {
           console.error(error.statusText);
       });
