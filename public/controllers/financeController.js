7dcfab04b034224f59a729b388b848f0a335f8ff
cammls <camille.malis@coding-academy.fr>
2017-02-06 18:12:16 +0100

    view finance en cours

--- public/controllers/financeController.js
+++ public/controllers/financeController.js
@@ -1,10 +1,11 @@
 app.controller('financeCtrl', ['$scope','dataService', function($scope, dataService) {
-  $scope.testeuh = "testeuheuhueh"
   $scope.getStripeData = function(){
     dataService.getStripeData()
     .then(function mySucces(response) {
-          $scope.stripeData = response.data;
-          console.log(response.data)
+          // $scope.stripeData = response.data;
+          $scope.stripeBalance = response.data.balance;
+          $scope.stripeCharges = response.data.charges;
+          $scope.stripeCustomers = response.data.customers;
       }, function myError(error) {
           console.error(error.statusText);
       });
