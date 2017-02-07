3332bc682ff07f518f6721bb0f3e0e46dfa8ff2b
Tom Modeste <tom.modeste@gmail.com>
2017-02-07 19:10:11 +0100

    pas fameux pour la fame de benoit

--- public/controllers/salesController.js
+++ public/controllers/salesController.js
@@ -1,12 +1,32 @@
 app.controller('salesCtrl', ['$scope','dataService', function($scope, dataService) {
+  // MAIN
+  $scope.income = 0;
   $scope.getPipedriveData = function(){
     dataService.getPipedriveData()
     .then(function mySucces(response) {
-          $scope.pipedriveData = response.data;
-          console.log('success');// console.log(response.data);
-      }, function myError(error) {
-          console.log('failure');// console.error(error.statusText);
-      });
-  }
-  $scope.getPipedriveData();
+      console.log(response.data)
+      $scope.pipedriveData = response.data;
+      for (var i = 0; i < response.data.pipedrive_json.length; i++) {
+        if (response.data.pipedrive_json[i].status === 'won') {
+          console.log(response.data.pipedrive_json[i].status);
+          $scope.income += response.data.pipedrive_json[i].value;
+        }
+      }
+    }, function myError(error) {
+    console.error(error.statusText);
+  });
+}
+
+$scope.getPipedriveData();
 }])
+// $scope.calculateSum = function(data){
+//   var sum=0;
+//   var counter=0;
+//   for (var property in data) {
+//     if (data.hasOwnProperty(property)) {
+//       sum += data[property];
+//       counter++;
+//     }
+//   }
+//   return sum;
+// };
