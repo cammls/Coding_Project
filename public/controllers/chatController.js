7b956db153a7a98059d843aa42dd193f3902cfaf
benoit nguyen <nguyen.hl.b@gmail.com>
2017-02-08 16:10:26 +0100

    receiving data from chatbox on server side

--- public/controllers/chatController.js
+++ public/controllers/chatController.js
@@ -1,28 +1,16 @@
 app.controller('chatCtrl', ['$scope','$log', 'chatSocket', 'messageFormatter', 'nickName', function($scope, $log, chatSocket, messageFormatter, nickName) {
 
   $scope.nickName = nickName
-  $scope.messageLog = 'Ready to chat!'
+  $scope.data = {
+    message : ""
+  }
 
   // OSEF UN PEU DES NICKNAME, A SETTER SELON LE NOM DU CURRENT USER QUOI
-  $scope.sendMessage = function() {
-    var match = $scope.message.match('^\/nick (.*)')
-
-    if (angular.isDefined(match) &&
-        angular.isArray(match) && match.length === 2) {
-      var oldNick = nickName
-      nickName = match[1]
-      $scope.message = ''
-      $scope.messageLog = messageFormatter(new Date(),
-                      nickName, 'nickname changed - from ' +
-                      oldNick + ' to ' + nickName + '!') +
-                      $scope.messageLog
-      $scope.nickName = nickName
-    }
-
-    $log.debug('sending message', $scope.message)
-    chatSocket.emit('message', nickName, $scope.message)
-    $log.debug('message sent', $scope.message)
-    $scope.message = ''
+  $scope.sendMessage = function(message) {
+    $log.debug('sending message', message)
+    chatSocket.emit('message', nickName, message)
+    $log.debug('message sent', message)
+    $scope.data.message = ""
   }
 
   $scope.$on('socket:broadcast', function(event, data) {
