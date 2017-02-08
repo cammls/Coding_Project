500336b8ac79a71ea66d128b189a11408f0a4de6
cammls <camille.malis@coding-academy.fr>
2017-02-08 19:14:18 +0100

    merge

--- public/controllers/profileController.js
+++ public/controllers/profileController.js
@@ -1,4 +1,3 @@
 app.controller('profileCtrl', ['$scope', 'authService', '$location', '$state', function($scope, authService, $location, $state) {
-  $scope.test=[1,2,3];
-  
+  // TODO
 }])
--- public/controllers/registerController.js
+++ public/controllers/registerController.js
@@ -11,8 +11,7 @@ app.controller('registerCtrl', ['$scope', 'authService', '$location', '$state', 
       authService
         .register(vm.credentials)
         .catch(function(err){
-          console.log(err.message);
-          console.log(err.data);
+          console.log(err)
         })
         .then(function(){
           $state.go('createCompany')
